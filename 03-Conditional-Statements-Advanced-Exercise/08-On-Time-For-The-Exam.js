90133f0f5ac5f693d1cd12ba7f47d409b8af1b9b
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:10:31 +0100

    Add files via upload

---
+++ 03-Conditional-Statements-Advanced-Exercise/08-On-Time-For-The-Exam.js
@@ -0,0 +1,34 @@
+function onTimeForExam(input) {
+    const examHour = Number(input[0]);
+    const examMinutes = Number(input[1]);
+    const arrivalHour = Number(input[2]);
+    const arrivalMinutes = Number(input[3]);
+
+    const normalizedExamTime = examHour * 60 + examMinutes;
+    const normalizedArrivalTime = arrivalHour * 60 + arrivalMinutes;
+
+    const diffInMinutes = normalizedExamTime - normalizedArrivalTime;
+
+    if (diffInMinutes > 30) console.log("Early");
+    else if (diffInMinutes >= 0) console.log("On time");
+    else console.log("Late");
+
+    if (diffInMinutes != 0) {
+        const absoluteDiffInMinutes = Math.abs(diffInMinutes);
+        let resultText = "";
+
+        if (absoluteDiffInMinutes < 60) resultText += `${absoluteDiffInMinutes} minutes`;
+        else {
+            const formattedHours = Math.floor(absoluteDiffInMinutes / 60);
+            const formattedMinutes = absoluteDiffInMinutes % 60;
+
+            resultText += `${formattedHours}:${formattedMinutes.toString().padStart(2, "0")} hours`;
+        }
+
+        if (diffInMinutes > 0) resultText += " before";
+        else resultText += " after";
+
+        resultText += " the start";
+        console.log(resultText);
+    }
+}
