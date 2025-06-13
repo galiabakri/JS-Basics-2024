3caf42d6cdfd6433171e9a8e775c7d1e6959f6d8
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:14:55 +0100

    Add files via upload

---
+++ 03-Conditional-Statements-Advanced-Lab/07-Working-Hours.js
@@ -0,0 +1,10 @@
+function workingHours(input) {
+    const hour = Number(input[0]);
+    const day = input[1];
+
+    if (day != "Sunday" && hour >= 10 && hour <= 18) {
+        console.log("open");
+    } else {
+        console.log("closed");
+    }
+}
