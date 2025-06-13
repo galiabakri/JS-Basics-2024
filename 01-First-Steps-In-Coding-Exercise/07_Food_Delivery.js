25a5ac03232e52a61a51709353eb91cd37bf6e38
galiabakri <galia.bakri@gmail.com>
2025-06-13 16:58:09 +0100

    Initial commit

--- 01-First-Steps-In-Coding-Exercise/07_Food_Delivery.js
+++ 01-First-Steps-In-Coding-Exercise/07_Food_Delivery.js
@@ -3,7 +3,7 @@ function foodDelivery(input) {
     let fishMenu = Number(input[1]);
     let vegetarianMenu = Number(input[2]);
 
-    let totalSum = chickenMenu * 10.35 + fishMenu * 12.40 + vegetarianMenu * 8.15;
+    let totalSum = chickenMenu * 10.30 + fishMenu * 12.40 + vegetarianMenu * 8.15;
     let totalSumWithDessert = totalSum * 1.20;
     let finalSum = totalSumWithDessert + 2.50;
 
