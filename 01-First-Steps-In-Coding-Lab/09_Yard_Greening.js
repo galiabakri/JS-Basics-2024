418275b09534ae438783d0c501e030fa54858edb
galiabakri <galia.bakri@gmail.com>
2025-06-13 17:18:08 +0100

    Delete 01-First-Steps-In-Coding-Lab/09_Yard_Greening.js

--- 01-First-Steps-In-Coding-Lab/09_Yard_Greening.js
+++
@@ -1,9 +0,0 @@
-function yardGreening(input) {
-    let squaredMeters = Number(input[0]);
-
-    let totalCost = squaredMeters * 7.61;
-    let discount = totalCost * 0.18;
-
-    console.log(`The final price is: ${totalCost - discount} lv.`);
-    console.log(`The discount is: ${discount} lv.`);
-}
