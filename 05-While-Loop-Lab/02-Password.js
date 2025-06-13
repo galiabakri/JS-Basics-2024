ffae883e3a14a61f2a0c7be9463085f1b92041dd
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:24:48 +0100

    Delete 05-While-Loop-Lab/02-Password.js

--- 05-While-Loop-Lab/02-Password.js
+++
@@ -1,9 +0,0 @@
-function password(input) {
-    const username = input[0];
-    const password = input[1];
-
-    let index = 2;
-    while (input[index] !== password) index++;
-
-    console.log(`Welcome ${username}!`);
-}
