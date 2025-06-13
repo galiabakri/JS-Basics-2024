1708417413b21a8f36f8ec1358822b4bfe93655a
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:03:23 +0100

    Add files via upload

---
+++ 02-Conditional-Statements-Lab/01-Excellent-Result.js
@@ -0,0 +1,8 @@
+function excellentResult(input) {
+    let result = Number(input[0]);
+    let excellentMessage = 'Excellent!'
+
+    if (result >= 5.50) {
+        console.log(excellentMessage);
+    }
+}
