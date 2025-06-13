ae85616e802182ad5db0cbfda922de60dda02820
galiabakri <galia.bakri@gmail.com>
2025-06-13 17:11:50 +0100

    Delete 01-First-Steps-In-Coding-Exercise/02_Radians_to_Degrees.js

--- 01-First-Steps-In-Coding-Exercise/02_Radians_to_Degrees.js
+++
@@ -1,8 +0,0 @@
-function radiansToDegrees(input) {
-    let radiansString = input[0];
-    let radians = Number(radiansString);
-
-    let degrees = radians * 180 / Math.PI;
-
-    console.log(degrees);
-}
