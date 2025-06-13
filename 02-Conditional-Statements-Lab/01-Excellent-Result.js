1886943d9fb1ac342ac5e5fb68f5be7d93721e13
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:01:22 +0100

    Delete 02-Conditional-Statements-Lab/01-Excellent-Result.js

--- 02-Conditional-Statements-Lab/01-Excellent-Result.js
+++
@@ -1,8 +0,0 @@
-function excellentResult(input) {
-    let result = Number(input[0]);
-    let excellentMessage = 'Excellent!'
-
-    if (result >= 5.50) {
-        console.log(excellentMessage);
-    }
-}
