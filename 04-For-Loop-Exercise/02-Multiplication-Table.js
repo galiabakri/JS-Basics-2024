4fcc5d27b078e7e90aa602fce5b532c3040ab845
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:15:44 +0100

    Delete 04-For-Loop-Exercise/02-Multiplication-Table.js

--- 04-For-Loop-Exercise/02-Multiplication-Table.js
+++
@@ -1,9 +0,0 @@
-function multiplicationTable(input) {
-    let numberToMultiply = Number(input[0]);
- 
-    for (let i = 1; i <= 10; i++) {
-        let result = i * numberToMultiply;
- 
-        console.log(`${i} * ${numberToMultiply} = ${result}`);
-    }
-}
