cee4033a3b852579e96386f78b88c9468941e770
galiabakri <galia.bakri@gmail.com>
2025-06-13 17:12:04 +0100

    Delete 01-First-Steps-In-Coding-Exercise/03_Deposit_Calculator.js

--- 01-First-Steps-In-Coding-Exercise/03_Deposit_Calculator.js
+++
@@ -1,9 +0,0 @@
-function depositCalculator(input) {
-    let depositAmount = Number(input[0]);
-    let depositTime = Number(input[1]);
-    let interestRate = Number(input[2]) / 100;
-
-    let depositAfter = depositAmount + depositTime * ((depositAmount * interestRate) / 12);
-
-    console.log(depositAfter);
-}
