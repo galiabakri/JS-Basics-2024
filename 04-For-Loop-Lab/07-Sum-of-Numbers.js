0fc54ce0c872a534ce52b22a6d3bbaa9493b051e
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:19:31 +0100

    Delete 04-For-Loop-Lab/07-Sum-of-Numbers.js

--- 04-For-Loop-Lab/07-Sum-of-Numbers.js
+++
@@ -1,8 +0,0 @@
-function sumOfNumbers(input) {
-    const number = input[0];
-
-    let sum = 0;
-    for (let i = 0; i < number.length; i++) sum += number[i] - "0";
-
-    console.log(`The sum of the digits is:${sum}`);
-}
