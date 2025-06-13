dca7dabbc08b739b64aa14346aa3f444f8ae1c63
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:25:00 +0100

    Delete 05-While-Loop-Lab/03-Sum-Numbers.js

--- 05-While-Loop-Lab/03-Sum-Numbers.js
+++
@@ -1,9 +0,0 @@
-function sumNumbers(input) {
-    const limit = Number(input[0]);
-
-    let sum = 0;
-    let index = 1;
-    while (sum < limit) sum += Number(input[index++]);
-
-    console.log(sum);
-}
