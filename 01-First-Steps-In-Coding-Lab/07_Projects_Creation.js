b5cac0dd7201c1d833ed2384cd742427728d3524
galiabakri <galia.bakri@gmail.com>
2025-06-13 17:17:38 +0100

    Delete 01-First-Steps-In-Coding-Lab/07_Projects_Creation.js

--- 01-First-Steps-In-Coding-Lab/07_Projects_Creation.js
+++
@@ -1,8 +0,0 @@
-function projectsCreation(input) {
-    let architect = input[0];
-    let projectsCount = Number(input[1]);
-    let hoursNeeded = projectsCount * 3;
-
-    let result = `The architect ${architect} will need ${hoursNeeded} hours to complete ${projectsCount} project/s.`
-    console.log(result);
-}
