c1ef551230ba33516f8690eafcc4214fe8c12927
galiabakri <galia.bakri@gmail.com>
2025-06-13 17:18:41 +0100

    Add files via upload

---
+++ 01-First-Steps-In-Coding-Lab/09_Yard_Greening.js
@@ -0,0 +1,9 @@
+function yardGreening(input) {
+    let squaredMeters = Number(input[0]);
+
+    let totalCost = squaredMeters * 7.61;
+    let discount = totalCost * 0.18;
+
+    console.log(`The final price is: ${totalCost - discount} lv.`);
+    console.log(`The discount is: ${discount} lv.`);
+}
