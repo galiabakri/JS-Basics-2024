ccfb8508200830e27697ff921aea6e844c6f2d6f
galiabakri <galia.bakri@gmail.com>
2025-06-13 16:59:48 +0100

    Initial commit

--- 01-First-Steps-In-Coding-Exercise/06_Repainting.js
+++ 01-First-Steps-In-Coding-Exercise/06_Repainting.js
@@ -4,7 +4,7 @@ function repainting(input) {
     let diluent = Number(input[2]);
     let workHours = Number(input[3]);
 
-    let bagsSum = 0.30;
+    let bagsSum = 0.40;
 
     let totalSum = plastic * 1.50 + paint * 14.50 + diluent * 5.00 + bagsSum;
     let workerSum = totalSum * 0.30 * workHours;
