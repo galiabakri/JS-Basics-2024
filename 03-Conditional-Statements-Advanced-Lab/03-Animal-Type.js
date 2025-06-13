1fbd69b099dc8c592d16c195bf8def8eea5a6a1d
galiabakri <galia.bakri@gmail.com>
2025-06-13 18:12:19 +0100

    Delete 03-Conditional-Statements-Advanced-Lab/03-Animal-Type.js

--- 03-Conditional-Statements-Advanced-Lab/03-Animal-Type.js
+++
@@ -1,7 +0,0 @@
-function AnimalType(input) {
-    const animal = input[0];
-
-    if (animal === "dog") console.log("mammal");
-    else if (animal === "crocodile" || animal === "tortoise" || animal === "snake") console.log("reptile");
-    else console.log("unknown");
-}
